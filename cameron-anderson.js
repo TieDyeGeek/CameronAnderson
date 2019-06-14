d76f6dfe2d14b8f3a2b43e33a07cb7b066636e18
TieDyeGeek <canderson3.14@me.com>
2019-06-13 19:27:57 -0500

    Initial commit

---
+++ cameron-anderson.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+	loadHeader();
+	loadFooter();
+}
+
+
+function loadHeader(){
+	var header = document.getElementById("header");
+	load("header.html", header)
+}
+
+function loadFooter(){
+	var footer = document.getElementById("footer");
+	load("footer.html", footer)
+}
+
+
+function load(url, element)
+{
+    req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+
+    element.innerHTML = req.responseText; 
+}
