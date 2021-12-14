916f84a5eb0c0da8fcf56e4fb58196625ad5c04e
TieDyeGeek <canderson3.14@me.com>
2021-12-13 22:19:28 -0600

    add RestaurantLevels page

---
+++ src/CameronAnderson.Restaurants/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿namespace CameronAnderson.Restaurants
+{
+	public class Restaurant 
+	{
+		public Restaurant(string name, double level)
+		{
+			Name = name;
+			Level = level;
+		}
+
+		public string Name { get; set; }
+		public double Level {  get; set; }
+	}
+}
---
+++ src/CameronAnderson.Restaurants/RestaurantInteractor.cs
@@ -0,0 +1,26 @@
+﻿namespace CameronAnderson.Restaurants
+{
+	public static class RestaurantInteractor
+	{
+		public static List<Restaurant> GetRestaurants()
+		{
+			return new List<Restaurant>
+			{
+				new Restaurant("McDonald's", 1),
+				new Restaurant("Burger King", 1),
+				new Restaurant("Wendy's", 1),
+				new Restaurant("Jimmy Johns", 1.5),
+				new Restaurant("Subway", 1.5),
+				new Restaurant("Portillos", 1.5),
+				new Restaurant("Chipotle", 1.5),
+				new Restaurant("Dominos", 1.5),
+				new Restaurant("Chinese Buffets", 2),
+				new Restaurant("Buffalo Wild Wings", 2),
+				new Restaurant("Kenny's Westside Pub", 2),
+				new Restaurant("Olive Garden", 3),
+				new Restaurant("Blue Duck", 3),
+				new Restaurant("Alexander's Steakhouse", 4)
+			};
+		}
+	}
+}
