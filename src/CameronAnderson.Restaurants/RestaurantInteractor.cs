8094cc9c008ddeccd8901078e63936f7f83232d5
Cameron Anderson <canderson3.14@me.com>
2022-12-04 15:47:19 -0600

    Update restaurant levels and add some more

--- src/CameronAnderson.Restaurants/RestaurantInteractor.cs
+++ src/CameronAnderson.Restaurants/RestaurantInteractor.cs
@@ -18,11 +18,10 @@ public static class RestaurantInteractor
 
 	private static List<RestaurantLevel> Levels => new()
 	{
-		new RestaurantLevel(1, "Fast Food (order at front)"),
-		new RestaurantLevel(1.5, "Semi-Fast Food"),
-		new RestaurantLevel(2, "Mom & Pops / Sports Bars (sit downs)"),
-		new RestaurantLevel(3, "Full Sit Downs (no TVs)"),
-		new RestaurantLevel(4, "Fancy (specialty items; focus)"),
+		new RestaurantLevel(1, "Fast Food (Generally take out)"),
+		new RestaurantLevel(2, "Fast Casual (Take out or eat in; pay when ordering"),
+		new RestaurantLevel(3, "Sit Downs"),
+		new RestaurantLevel(4, "Fancy"),
 	};
 
 	private static List<Restaurant> Restaurants => new()
@@ -30,23 +29,28 @@ public static class RestaurantInteractor
 		new Restaurant("McDonald's", 1),
 		new Restaurant("Burger King", 1),
 		new Restaurant("Wendy's", 1),
-		new Restaurant("Jimmy John's", 1.5),
-		new Restaurant("Subway", 1.5),
-		new Restaurant("Portillo's", 1.5),
-		new Restaurant("Chipotle", 1.5),
-		new Restaurant("Domino's", 1.5),
-		new Restaurant("Culver's", 1.5),
-		new Restaurant("Chinese Buffets", 2),
-		new Restaurant("Buffalo Wild Wings", 2, "https://www.buffalowildwings.com/en/food/"),
-		new Restaurant("Kenny's Westside Pub", 2),
+		new Restaurant("Subway", 1),
+		new Restaurant("Domino's", 1),
+
+		new Restaurant("Jimmy John's", 2),
+		new Restaurant("Portillo's", 2),
+		new Restaurant("Chipotle", 2),
+		new Restaurant("Culver's", 2),
+		new Restaurant("Five Guys", 2),
+		new Restaurant("LaGondola", 2, "https://lagondolaspaghettihouse.com/"),
+		new Restaurant("China Garden", 2, "http://www.chinagardengermantownhills.com"),
 		new Restaurant("Noodles and Company", 2),
+
+		new Restaurant("Buffalo Wild Wings", 3, "https://www.buffalowildwings.com/en/food/"),
+		new Restaurant("Kenny's Westside Pub", 3),
 		new Restaurant("Olive Garden", 3),
 		new Restaurant("Applebee's", 3),
-		new Restaurant("Blue Duck", 3),
+		new Restaurant("Blue Duck", 3, "http://www.blueduckbarbecue.com/menu"),
 		new Restaurant("Texas Road House", 3),
+		new Restaurant("Kelleher's", 3, "https://www.kellehersonwater.com/food"),
+		new Restaurant("Maquet's Rail House", 3, "https://maquetsrailhouse.com/menu"),
+		new Restaurant("Seasons Gastropub", 3, "https://www.seasonsgastro.pub/menus/"),
+
 		new Restaurant("Alexander's Steakhouse", 4),
-		new Restaurant("Kelleher's", 2, "https://www.kellehersonwater.com/food"),
-		new Restaurant("Maquet's Rail House", 2, "https://maquetsrailhouse.com/menu"),
-		new Restaurant("Seasons Gastropub", 3, "https://www.seasonsgastro.pub/menus/")
 	};
 }
